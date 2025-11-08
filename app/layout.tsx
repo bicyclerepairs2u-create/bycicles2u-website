492e174e286ba8d538dece420ebc27455abb2079
Dashiell Russell <dashiell.russell@gmail.com>
2025-11-08 13:46:16 +1100

    feat: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Roboto } from "next/font/google"
 import "./globals.css"
+import { Analytics } from "@vercel/analytics/next"
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -29,6 +30,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css" />
       </head>
       <body>{children}</body>
+      <Analytics/>
     </html>
   )
 }
